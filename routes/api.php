f267658574adb41e769f7037a6ed4c46381c6f94
Ayodeji <ayodejiolaniran19@gmail.com>
2025-02-24 17:46:52 +0100

    voyatek

--- routes/api.php
+++ routes/api.php
@@ -12,25 +12,26 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\CommentController;
 
-Route::middleware(['token'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::get('/blogs', [BlogController::class, 'index']);
-    Route::get('/blogs/{id}', [BlogController::class, 'show']);
-    Route::put('/blogs/{id}', [BlogController::class, 'update']);
-    Route::delete('/blogs/{id}', [BlogController::class, 'destroy']);
-
-    Route::get('/blogs/{blogId}/posts', [PostController::class, 'index']);
-    Route::post('/blogs/{blogId}/posts', [PostController::class, 'store']);
-    Route::get('/posts/{id}', [PostController::class, 'show']);
-    Route::put('/posts/{id}', [PostController::class, 'update']);
-    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
-
-    Route::post('posts/{postId}/like', [LikeController::class, 'likePost']);
-    Route::post('posts/{postId}/comment', [CommentController::class, 'commentPost']);
+// Route::middleware(['token'])->group(function () {
+Route::get('/user', function (Request $request) {
+    return $request->user();
 });
+
+Route::get('/blogs', [BlogController::class, 'index']);
+Route::post('/blogs', [BlogController::class, 'store']);
+Route::get('/blogs/{id}', [BlogController::class, 'show']);
+Route::put('/blogs/{id}', [BlogController::class, 'update']);
+Route::delete('/blogs/{id}', [BlogController::class, 'destroy']);
+
+Route::get('/blogs/{blogId}/posts', [PostController::class, 'index']);
+Route::post('/blogs/{blogId}/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+Route::post('posts/{postId}/like', [LikeController::class, 'likePost']);
+Route::post('posts/{postId}/comment', [CommentController::class, 'commentPost']);
+// });
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
